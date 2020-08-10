c18b0d1552eb883a45a92fe9c569c6bdbb769b84
malhotra-parul <parulm.business@gmail.com>
2020-08-10 22:29:59 +0530

    created the profile card component

---
+++ src/components/ProfileCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function ProfileCard() {
+    return (
+        <div class="profile">
+           <a href="https://ibb.co/xMfWZWL" style={{paddingBottom: "10px"}}>
+               <img src="https://i.ibb.co/xMfWZWL/michael-dam-m-EZ3-Po-FGs-k-unsplash.jpg" 
+               alt="michael-dam-m-EZ3-Po-FGs-k-unsplash" border="0" className="picture"/></a> 
+           <h4>Martha Blair  <div className="online space"></div></h4>
+            <p style={{paddingTop: "10px"}}>Developer</p>
+        </div>
+    )
+}
+
+export default ProfileCard
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import ProfileCard from "./ProfileCard"
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            
+            <ProfileCard />
         </div>
     )
 }
